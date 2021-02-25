a970d4b2b237861e3ed44add02983185dd7031b7
Refik95 <refikerk@gmail.com>
2021-02-25 16:02:21 +0300

    24 şubat ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -7,8 +7,12 @@ namespace Business.Constants
     public static class Messages
     {
         public static string ProductAdded = "Ürün eklendi";
+        public static string ProductUpdated = "Ürün güncellendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem bakımda";
         public static string ProductsListed="Ürünler listelendi";
+        public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Bu isimde başka ürün zaten var";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
